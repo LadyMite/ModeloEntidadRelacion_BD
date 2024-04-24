734e8fe5a5696e710e20c27b581a97eea7b67462
LadyMite <165735723+LadyMite@users.noreply.github.com>
2024-04-24 15:25:58 -0500

    DB_AhsokaVoice.sql en SQL server

--- BD/DB_AhsokaVoice.sql
+++ BD/DB_AhsokaVoice.sql
@@ -1,72 +1,58 @@
---
--- PostgreSQL database dump
---
+-- Crear la base de datos DB_AhsokaVoice
+CREATE DATABASE DB_AhsokaVoice;
+GO
 
--- Dumped from database version 16.2 (Debian 16.2-1.pgdg120+2)
--- Dumped by pg_dump version 16.2 (Debian 16.2-1.pgdg120+2)
+-- Usar la base de datos DB_AhsokaVoice
+USE DB_AhsokaVoice;
+GO
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: demo; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.demo (
-    id integer NOT NULL,
-    name character varying(200) DEFAULT ''::character varying NOT NULL,
-    hint text DEFAULT ''::text NOT NULL
+-- Crear la tabla Usuarios
+CREATE TABLE Usuario (
+    IdUsuario INT PRIMARY KEY IDENTITY,
+    nombreUsuario VARCHAR(100),
+    contraseniaUsuario VARCHAR(100),
+    correoUsuario VARCHAR(100),
+    fechaNacUsuario DATE
 );
+GO
+
+-- Crear la tabla Areas
+CREATE TABLE Area (
+    IdArea INT PRIMARY KEY IDENTITY,
+    nombreArea VARCHAR(100)
+);
+GO
+
+-- Crear la tabla Logros
+CREATE TABLE Logro (
+    IdLogro INT PRIMARY KEY IDENTITY,
+    tituloLogro VARCHAR(100),
+    fechaLogro DATE,
+    IdArea INT,
+    descripcionLogro VARCHAR(MAX),
+    imagenLogro VARBINARY(MAX),
+    IdUsuario INT,
+    FOREIGN KEY (IdArea) REFERENCES Area(IdArea),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario)
+);
+GO
 
 
---
--- Name: demo_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.demo_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: demo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.demo_id_seq OWNED BY public.demo.id;
-
-
---
--- Name: demo id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.demo ALTER COLUMN id SET DEFAULT nextval('public.demo_id_seq'::regclass);
-
-
---
--- Name: demo demo_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.demo
-    ADD CONSTRAINT demo_pkey PRIMARY KEY (id);
-
+-- Crear la tabla Likes
+CREATE TABLE Likes (
+    ID_Like INT PRIMARY KEY IDENTITY,
+    ID_Logro INT,
+    ID_Usuario INT,
+    FOREIGN KEY (ID_Logro) REFERENCES Logro(IdLogro),
+    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(IdUsuario)
+);
+GO
 
 --
--- PostgreSQL database dump complete
---
+USE master;
+GO
+
+-- Eliminar la base de datos DB_AhsokaVoice
+DROP DATABASE DB_AhsokaVoice;
+GO
 
